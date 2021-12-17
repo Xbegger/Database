0023fd6551053c9cfe6e77ccbc417946b723d8c0
TheKing_Shun <962262149@qq.com>
2021-12-17 19:44:53 +0800

    上传GO语言开发相关资料

---
+++ Go开发/cookbook-LeetCode/leetcode tmp/leetcode/editor/cn/[剑指 Offer II 018]有效的回文.go
@@ -0,0 +1,45 @@
+//给定一个字符串 s ，验证 s 是否是 回文串 ，只考虑字母和数字字符，可以忽略字母的大小写。 
+//
+// 本题中，将空字符串定义为有效的 回文串 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "A man, a plan, a canal: Panama"
+//输出: true
+//解释："amanaplanacanalpanama" 是回文串 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "race a car"
+//输出: false
+//解释："raceacar" 不是回文串 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 2 * 10⁵ 
+// 字符串 s 由 ASCII 字符组成 
+// 
+//
+// 
+//
+// 注意：本题与主站 125 题相同： https://leetcode-cn.com/problems/valid-palindrome/ 
+// Related Topics 双指针 字符串 👍 2 👎 0
+
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+func isPalindrome(s string) bool {
+	n := len(s)
+	l , r := 0,n-1
+	for l < r {
+
+	}
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
